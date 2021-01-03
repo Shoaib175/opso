1cb8c1036bbc518b722cc5fe5b3de2280a9ac3f1
Shoaib175 <shaikhshoaib175@gmail.com>
2021-01-03 20:08:52 +0530

    Scraper

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 from re import T
 import streamlit as st
-
-
-def showpositiveprompt():
-    st.success('Success')
-
-def shownegprompt():
-    st.error('Failed!')
-
-def validation(username):
-    if (username=='shoaib'):
-        showpositiveprompt()
-    else:
-        shownegprompt()
-
+import streamlit.components.v1 as components
+import time
+import requests
+from bs4 import BeautifulSoup
 
 def main():
     st.title('On-Page SEO Optimizer')
     st.subheader('Combining data science with SEO to get better results!')
-    st.subheader('Automatic Deployment')
-    username = st.text_input("Enter your name")
-    butt = st.button("button")
-    if(butt==T):
-        validation(username)
+    city = st.text_input("Enter your target keyword")
+    button = st.button("Scrape")
+    if(button==T):
+        url = "https://www.google.com/search?q=baby+shark"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html.parser")
+        st.write(req.text)
+        st.write(soup.title.text)   
+
 
 
 if __name__ == '__main__':
