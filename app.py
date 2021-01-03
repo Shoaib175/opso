804288aa6c99434b576c451040ef264ff78c8d38
Shoaib175 <shaikhshoaib175@gmail.com>
2021-01-03 15:44:22 +0530

    Final Release maybe

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+from re import T
 import streamlit as st
+
+
+def showpositiveprompt():
+    st.success('Success')
+
+def shownegprompt():
+    st.error('Failed!')
+
+def validation(username):
+    if (username=='shoaib'):
+        showpositiveprompt()
+    else:
+        shownegprompt()
+
 
 def main():
     st.title('On-Page SEO Optimizer')
     st.subheader('Combining data science with SEO to get better results!')
     st.subheader('Automatic Deployment')
+    username = st.text_input("Enter your name")
+    butt = st.button("button")
+    if(butt==T):
+        validation(username)
+
 
 if __name__ == '__main__':
     main()
