10c26e731c579d2be2099be2753e21d587981767
Shoaib175 <shaikhshoaib175@gmail.com>
2021-01-03 15:35:31 +0530

    Sixth Release

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 def main():
     st.title('On-Page SEO Optimizer')
     st.subheader('Combining data science with SEO to get better results!')
+    st.subheader('Automatic Deployment')
 
 if __name__ == '__main__':
     main()
